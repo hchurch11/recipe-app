89905642d0ab21e769569084c383d544a95d7dbb
hchurch11 <heatherlchurch11@gmail.com>
2020-12-01 12:20:05 -0700

    add recipe link/added bootstrap cards

--- src/Footer.js
+++ src/Footer.js
@@ -1,13 +1,14 @@
 import React from "react";
 import logo from "./logo.svg";
-import "./Footer.css";
 
 export default function Footer() {
   return (
     <div className="Footer text-center ">
       <p>
-        <a href="https://github.com/hchurch11/recipe-app">open-sourced</a> code
-        by Heather Church{" "}
+        <a className="App-link" href="https://github.com/hchurch11/recipe-app">
+          open-sourced
+        </a>{" "}
+        code by Heather Church{" "}
       </p>
       <p>made in React</p>
       <img src={logo} className="App-logo" alt="logo" />
--- src/Recipe.js
+++ src/Recipe.js
@@ -1,27 +1,45 @@
 import React from "react";
-import "./Recipe.css";
 
-const Recipe = ({ title, calories, image, ingredients, portions }) => {
+const Recipe = ({
+  title,
+  calories,
+  image,
+  ingredients,
+  portions,
+  totalTime,
+  recipeUrl,
+}) => {
   return (
-    <div className="Recipe text-center">
-      <div className="recipe-info">
-        <h2>{title}</h2>
+    <div className="Recipe text-center card">
+      <div className="recipe-info card-body">
+        <h2 className="card-title">{title}</h2>
         <h3 className="font-italic">Serves: {portions}</h3>
         <p> {Math.round(calories / portions)} cal/serving</p>
-      </div>
+        <img className="recipe-img card-img-top" src={image} alt=""></img>
 
-      <img className="recipe-img card-img-top" src={image} alt=""></img>
-      <div className="ingredients-list ">
-        <ol>
-          {ingredients.map((ingredients, index) => (
-            <li
-              className="ingredient text-left"
-              key={`${ingredients.text}_${index}`}
-            >
-              {ingredients.text}
-            </li>
-          ))}
-        </ol>
+        <div className="ingredients-list card-text">
+          <ol>
+            {ingredients.map((ingredients, index) => (
+              <li
+                className="ingredient text-left"
+                key={`${ingredients.text}_${index}`}
+              >
+                {ingredients.text}
+              </li>
+            ))}
+          </ol>
+          <a
+            href={recipeUrl}
+            target="_blank"
+            rel="noreferrer"
+            className="btn recipe-url btn-block"
+          >
+            Go To Recipe
+          </a>
+        </div>
+        <div className="card-footer">
+          <p>Total Time: {totalTime} mins</p>
+        </div>
       </div>
     </div>
   );
